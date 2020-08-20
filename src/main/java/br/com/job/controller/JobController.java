68e86a2205aa7990e22cdde1f7459a0c0c042004
Madson Santos <madsonsamsung@gmail.com>
2020-08-20 08:52:51 -0300

    Layout da página index.htm e configuração de rota inicial

--- src/main/java/br/com/job/controller/JobController.java
+++ src/main/java/br/com/job/controller/JobController.java
@@ -1,5 +1,15 @@
 package br.com.job.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class JobController {
     
+    @GetMapping("/")
+    public ModelAndView findAll() {
+        ModelAndView modelAndView = new ModelAndView("/index");
+        return modelAndView;
+    }
 }
