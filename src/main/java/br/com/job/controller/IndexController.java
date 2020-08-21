3eeb3f93593fce83869096fb990964d5f4c29225
Madson Santos <madsonsamsung@gmail.com>
2020-08-21 09:28:55 -0300

    Modularizando layout com Thymeleaf fragment

--- src/main/java/br/com/job/controller/IndexController.java
+++ src/main/java/br/com/job/controller/IndexController.java
@@ -2,21 +2,21 @@ package br.com.job.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.job.service.JobService;
 
 @Controller
-public class JobController {
-    
+public class IndexController {
+
     @Autowired
     private JobService jobService;
 
-    @RequestMapping(value = "/jobs", method = RequestMethod.GET)
-    public ModelAndView findAll(){
-        ModelAndView modelAndView = new ModelAndView("/jobs");
+    @RequestMapping("/")
+    public ModelAndView index(){
+        ModelAndView modelAndView = new ModelAndView("/index");
+        modelAndView.addObject("jobs", jobService.findAll());
         return modelAndView;
     }
 }
